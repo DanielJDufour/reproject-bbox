2fe0ea4d5a90e2e577ed194d4e4511775b575e92
DanielJDufour <daniel.j.dufour@gmail.com>
2023-02-28 21:35:59 -0500

    rebuilt

--- test.js
+++ test.js
@@ -2,7 +2,6 @@ const test = require("flug");
 const proj4 = require("proj4-fully-loaded");
 
 const reprojectBoundingBox = require("./reproject-bbox");
-const reprojectBoundingBoxPluggable = require("./pluggable");
 
 test("geographic to web mercator", ({ eq }) => {
   const bbox = reprojectBoundingBox({
@@ -16,14 +15,6 @@ test("geographic to web mercator", ({ eq }) => {
 
     // convert bounding box to this spatial reference system
     to: 3857,
-  });
-  eq(bbox, [-13637750.817083945, 5007917.677222896, -13618826.503649088, 5028580.202823918]);
-});
-
-test("geographic to web mercator (pluggable)", ({ eq }) => {
-  const bbox = reprojectBoundingBoxPluggable({
-    bbox: [-122.51, 40.97, -122.34, 41.11],
-    reproject: proj4("EPSG:4326", "EPSG:3857").forward,
   });
   eq(bbox, [-13637750.817083945, 5007917.677222896, -13618826.503649088, 5028580.202823918]);
 });
